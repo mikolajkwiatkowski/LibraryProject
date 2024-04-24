22293f1d3f5ab21059d318eb2417388c2a989464
Mikolaj Kwiatkowski <jestemczarny2137@gmail.com>
2024-04-25 01:24:54 +0200

    creating npm server

---
+++ first-react-project/src/Footer.jsx
@@ -0,0 +1,13 @@
+
+function Footer(){
+
+    return(
+        <footer>
+            <p>Author: Mikolaj Kwiatkowski &copy;  {new Date().getFullYear()}</p>
+        </footer>
+    );
+
+
+}
+
+export default Footer
